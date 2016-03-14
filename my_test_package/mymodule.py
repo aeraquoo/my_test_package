006f0427aa0138955042e83ab0f4d40163505cfb
Jaye Heffernan <jpeheffernan@gmail.com>
2016-03-14 14:46:31 +1100

    Add docstrings

--- my_test_package/mymodule.py
+++ my_test_package/mymodule.py
@@ -1,7 +1,26 @@
 def multiply(a, b):
+    """Multiply two numbers, a and b
+
+    A simple wrapper around the python `*` operator.  Calls `a * b`.
+    Multiplying two ints will return an int, otherwise float.
+
+    :param a: the first number
+    :param b: the second mumber
+    :type a: int,float,...
+    :type b: int,float,...
+    :return: a times b
+    :rtype: int,float,...
+    """
     return(a*b)
 
 def multiplication_table(n):
+    """Makes a multiplication table for n, n times x for x up to n
+
+    :param n: the number
+    :type n: int
+    :return: the multiplication table
+    :rtype: str
+    """
     table = ''
     for i in range(1, n+1):
         table += "{} times {} is {}\n".format(n, i, multiply(n, i))
